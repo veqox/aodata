e859084c3be401f0ab9c7f4b0f05c5be9a789785
VeQox <sonnleitner.luki@gmail.com>
2023-12-06 09:37:41 +0100

    add frontend project

---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,23 @@
+export interface MarketOrderCountByItem {
+    item_unique_name: string;
+    count: number;
+}
+
+export interface MarketOrderCountByLocation {
+    location: string;
+    count: number;
+}
+
+export interface MarketOrderCountByAuctionType {
+    auction_type: string;
+    count: number;
+}
+
+export interface MakretOrderCountByUpdatedAt {
+    updated_at: Date;
+    count: number;
+}
+
+export interface MarketOrderCount {
+    count: number;
+}
