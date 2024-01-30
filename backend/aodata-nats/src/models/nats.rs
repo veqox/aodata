2a0f500c092e510356fb401a66824a6496a48429
veqox <sonnleitner.luki@gmail.com>
2024-01-30 07:51:45 +0100

    feat: add market history nats model

--- backend/aodata-nats/src/models/nats.rs
+++ backend/aodata-nats/src/models/nats.rs
@@ -1,7 +1,5 @@
 use serde_json::Number;
 
-
-
 #[derive(serde::Deserialize)]
 pub struct MarketOrder {
     #[serde(rename = "Id")]
@@ -32,5 +30,38 @@ pub struct MarketOrder {
     pub auction_type: String,
 
     #[serde(rename = "Expires")]
-    pub  expires: String,
+    pub expires: String,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct MarketHistories {
+    #[serde(rename = "AlbionId")]
+    pub id: Number,
+
+    #[serde(rename = "AlbionIdString")]
+    pub item_id: String,
+
+    #[serde(rename = "LocationId")]
+    pub location_id: Number,
+
+    #[serde(rename = "QualityLevel")]
+    pub quality_level: Number,
+
+    #[serde(rename = "Timescale")]
+    pub timescale: Number,
+
+    #[serde(rename = "MarketHistories")]
+    pub market_histories: Vec<MarketHistory>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct MarketHistory {
+    #[serde(rename = "ItemAmount")]
+    pub item_amount: Number,
+
+    #[serde(rename = "SilverAmount")]
+    pub silver_amount: Number,
+
+    #[serde(rename = "Timestamp")]
+    pub timestamp: Number,
 }
